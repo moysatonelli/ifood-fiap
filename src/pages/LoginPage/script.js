bf3a0203d851eae6a7e9c805fabb97bc8ed805be
Leonardo Martin <leonardomartin.dev@gmail.com>
2024-08-27 08:53:29 -0300

    adding slide section

--- src/pages/LoginPage/script.js
+++ src/pages/LoginPage/script.js
@@ -1,14 +1,13 @@
 const togglePassword = document.querySelector(".toggle-password");
-const passwordInput = document.getElementById("password")
+const passwordInput = document.getElementById("password");
 
 togglePassword.addEventListener("click", () => {
-    togglePassword.classList.toggle("fa-regular")
-    togglePassword.classList.toggle("fa-eye")
-    togglePassword.classList.toggle("fa-regular")
-    togglePassword.classList.toggle("fa-eye-slash")
+  togglePassword.classList.toggle("fa-regular");
+  togglePassword.classList.toggle("fa-eye");
+  togglePassword.classList.toggle("fa-regular");
+  togglePassword.classList.toggle("fa-eye-slash");
 
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-})
-
-
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+});
